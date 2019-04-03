59e8db15dc66c5d6f99f2e33eec7c38dcc729ea9
mbkim95 <mbkim95@naver.com>
2019-04-04 01:31:03 +0900

    final test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,5 @@
 public class Test {
     public static void main(String[] args){
-        System.out.println("Hello Java!");
         System.out.println("Hello GitHub!");
 	    System.out.println("Hello Redmine!");
         System.out.println("Hello Jenkins!");
