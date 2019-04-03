0352ca2b56e11b34fb1c88d2bf01ccb06b68d3b3
mbkim95 <mbkim95@naver.com>
2019-04-04 00:55:05 +0900

    redmine test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,7 +2,6 @@ public class Test {
     public static void main(String[] args){
         System.out.println("Hello Java!");
         System.out.println("Hello GitHub!");
-        System.out.println("Hello JIRA!");
-	System.out.println("Hello redmine again!");
+	    System.out.println("Hello Redmine!");
     }
 }
