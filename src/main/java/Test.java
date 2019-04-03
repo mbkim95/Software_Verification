ce93dedf8c50af51bba23cd5b373da8e1c746bb1
mbkim95 <mbkim95@naver.com>
2019-04-03 20:46:58 +0900

    EAGT-1 JIRA TEST

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args){
         System.out.println("Hello Java!");
         System.out.println("Hello GitHub!");
+        System.out.println("Hello JIRA!");
     }
 }
