b6318df7150c0f9b3f090dd0b7b8ab7fc8484105
sysoy <sosoy2ee@gmail.com>
2019-04-04 02:24:51 +0900

    soyoung test push

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,5 +4,6 @@ public class Test {
 	    System.out.println("Hello Redmine!");
         System.out.println("Hello Jenkins!");
 	System.out.println("Hello Redmine at 02am");
+        System.out.println("God Minbeom am02:16");
     }
 }
