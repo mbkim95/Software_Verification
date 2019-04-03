92d7eff62941e30a58a3f25714757ac6aacce9e1
mbkim95 <mbkim95@naver.com>
2019-04-04 01:26:50 +0900

    test commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,5 +3,6 @@ public class Test {
         System.out.println("Hello Java!");
         System.out.println("Hello GitHub!");
 	    System.out.println("Hello Redmine!");
+        System.out.println("Hello Jenkins!");
     }
 }
