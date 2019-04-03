cfbd0cef972596fc2c9a8332f546cc86b9619f1d
Keonwoo <tmffka789@naver.com>
2019-04-04 02:03:03 +0900

    Modified Test.java by Keonwoo kim at 02am

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,5 +3,6 @@ public class Test {
         System.out.println("Hello GitHub!");
 	    System.out.println("Hello Redmine!");
         System.out.println("Hello Jenkins!");
+	System.out.println("Hello Redmine at 02am");
     }
 }
