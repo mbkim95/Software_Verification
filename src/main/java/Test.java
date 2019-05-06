d0149a0c2a42d567b20caa74d042a55e52c3f480
mbkim95 <mbkim95@naver.com>
2019-05-06 21:47:01 +0900

    Test Commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,7 +3,7 @@ public class Test {
         System.out.println("Hello GitHub!");
 	    System.out.println("Hello Redmine!");
         System.out.println("Hello Jenkins!");
-	System.out.println("Hello Redmine at 02am");
+	    System.out.println("Hello Redmine at 02am");
         System.out.println("God Minbeom am02:16");
     }
 }
