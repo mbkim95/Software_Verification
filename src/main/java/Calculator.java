7576ed2f2cf107c9ca7ab41ce86450da52b444d9
mbkim95 <mbkim95@naver.com>
2019-05-06 21:45:17 +0900

    Test Commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,9 @@
 public class Calculator {
-    public int add(int num1, int num2)
+    public int add(int num1, int num2) {
         return num1 + num2;
+    }
+
+    public int sub(int num1, int num2){
+        return num1 - num2;
+    }
 }
