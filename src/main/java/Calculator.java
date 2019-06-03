3495987467fff93ae2f055ea63abb10b751830e4
mbkim95 <mbkim95@naver.com>
2019-06-03 18:28:07 +0900

    test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -10,4 +10,8 @@ public class Calculator {
     public int mul(int num1, int num2){
         return num1 * num2;
     }
+
+    public int div(int num1, int num2){
+        return num1 / num2;
+    }
 }
